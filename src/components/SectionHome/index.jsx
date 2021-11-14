1d56efa377ca19f7ad36fbcc2be9b0ad2b8cd5c2
huynham <91304084+huynham@users.noreply.github.com>
2021-11-14 15:45:45 +0700

    first commit

---
+++ src/components/SectionHome/index.jsx
@@ -0,0 +1,34 @@
+
+import './home.css'
+
+
+function Home(){
+    
+    return (
+        <section id="home">
+            <div className="home-sub">
+                <div className="home-sub-one">
+                    <div className="home-content-1">
+                        <h1>We help startups launch their products</h1>
+                        <p>Etiam sed.Interdum consequat proin vestibulum class at a euismod mus luctus quam.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+                          
+                    </div>
+                    <div className="home-content-2">
+                        <div className="home-content-2-sub">
+                           <div>
+                           <iframe src="https://player.vimeo.com/video/69988283?h=374ede24b7"  frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+
+                           </div>
+                        </div>
+
+                    </div>
+                </div>
+                
+            </div>
+           
+            
+        </section>
+    )
+}
+export default Home
